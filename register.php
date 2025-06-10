54749b9f410973c70303211a4faf4840443f6e6a
MaJul <majul@gmail.com>
2025-06-10 20:48:29 +0800

    Add favicon link to multiple pages

--- register.php
+++ register.php
@@ -7,6 +7,7 @@
   <link href="bootsrap/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
   <script src="bootsrap/js/bootstrap.bundle.min.js"></script>
+  <link rel="icon" type="image/png" href="assets/logo.png">
 </head>
 <body class="bg-light">
   <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
--- restock.php
+++ restock.php
@@ -36,6 +36,7 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'
     <link href="bootsrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <script src="bootsrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="icon" type="image/png" href="assets/logo.png">
   </head>
   <body>
     <!-- modal-create-restock -->
